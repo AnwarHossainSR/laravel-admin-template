3ae412bd395f876d7fa4fd2a7382921219d95fea
Anwar Hossain <mahedianwar@gmail.com>
2025-01-11 20:30:55 +0600

    remove controllers

--- routes/web.php
+++ routes/web.php
@@ -2,25 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\AdminController;
-use App\Http\Controllers\AuthController;
-
-// Registration Routes
-Route::get('/register', [AuthController::class, 'showRegistrationForm'])->name('register');
-Route::post('/register', [AuthController::class, 'register']);
-
-// Sign In Routes
-Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
-Route::post('/login', [AuthController::class, 'login']);
-
-// Logout Route
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-
 Route::get('/', function () {
     return view('home');
 })->name('home');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/dashboard', [AdminController::class, 'dashboard']);
-    Route::get('/users', [AdminController::class, 'users']);
-});
